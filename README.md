88e1b5b51a238fad269ada1354537cc229fdbba6
Gabriel Bowen <gabe.bowen@utah.edu>
2022-07-05 13:34:24 -0600

    Update to CenoT

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CenoCO2
-Code and data for Cenozoic pCO2 curve fit
+# CenoT
+Code and data for Cenozoic deep sea temperature inversion analysis
--- code/diagnostics.R
+++
@@ -1,8 +0,0 @@
-load("out/post.rda")
-
-R2jags::traceplot(p, varname = c("pco2_m"))
-R2jags::traceplot(p, varname = c("pco2_m.pre", "pco2_m.eps.ac"))
-dev.off()
-
-View(p$BUGSoutput$summary)
-
